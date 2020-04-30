3e632689a283c0a158635ebfaac58a9c776333ce
abelender <jonuel.martiguez@gmail.com>
2020-04-30 18:47:21 +0200

    Was added a commented-out code description on the top of the file.

--- src/com/abelender/controlgasmatas/Price.java
+++ src/com/abelender/controlgasmatas/Price.java
@@ -1,5 +1,26 @@
 package com.abelender.controlgasmatas;
 
+/*
+ * Esta clase define un objeto que guarda información de los elementos 
+ * definida en el cuerpo del archivo xml ubicado en: https://bit.ly/2JNcTha. 
+ * 
+ * 		<places>		
+ *			 <place place_id="11703">
+ *   			<gas_price type="regular">14.99</gas_price>
+ *   			<gas_price type="premium">15.99</gas_price>
+ * 			 </place>
+ * 			.
+ * 			.
+ * 			.
+ * 			.
+ *      </places>
+ * 
+ * 			+ String gas_price: Guarda el valor definido dentro de la etiqueta <gas_price>
+ * 			+ String type:		Guarda el valor del atributo 'type' de la etiqueta <gas_price>
+ * 			+ String place_id:	Guarda el valor del atributo 'place_id'de la etiqueta <place>
+ * 
+ * */
+
 public class Price {
 	
 	private String gas_price;
@@ -16,9 +37,7 @@ public class Price {
 		this.place_id = place_id;
 		this.gas_price = gas_price;
 		this.type = type;
-		this.place = place;
-		
-		
+		this.place = place;	
 	}
 
 	public void setGas_price(String temp) {
@@ -62,9 +81,6 @@ public class Price {
 		  sb.append("Precio: " + gas_price + ".");
 
 		  return sb.toString();
-
 	}
-	
-
 }
 
